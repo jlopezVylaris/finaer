038dae96cefbbe46fa4629d0f9bf14f530191973
Joaquín López <jlopez@vylaris.ar>
2025-09-09 10:43:05 -0300

    cambios visuales en los distintas pages

--- finaer/.nuxt/dist/server/client.manifest.mjs
+++ finaer/.nuxt/dist/server/client.manifest.mjs
@@ -1,15 +1,1 @@
-export default {
-  "@vite/client": {
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "C:/Users/JOAQUIN/OneDrive/Desktop/programing/Trabajo/finaer/finaer/node_modules/nuxt/dist/app/entry.mjs": {
-    "resourceType": "script",
-    "module": true,
-    "isEntry": true,
-    "file": "C:/Users/JOAQUIN/OneDrive/Desktop/programing/Trabajo/finaer/finaer/node_modules/nuxt/dist/app/entry.mjs"
-  }
-}
+export { default } from "file:///C:/Users/JOAQUIN/OneDrive/Desktop/programing/Trabajo/finaer/finaer/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
